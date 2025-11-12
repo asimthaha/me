ae08350e3313bf718dbf444b87c09d0e606e504e
Asim Thaha <iamasimthaha@gmail.com>
2025-11-12 22:24:14 +0530

    edited index file

--- src/components/ParticlePortrait.tsx
+++ src/components/ParticlePortrait.tsx
@@ -14,7 +14,9 @@ class Particle {
   static swirlRotationSpeed: number = 0.0056;
   static swirlRandomWalkSpeed: number = 0.8;
   static swirlOscillation: number = 0.03;
-  static imageUrl: string = "/public/images/portrait.png";
+  static imageUrl: string = `${
+    import.meta.env.BASE_URL
+  }images/profile-image.png`;
 
   // --- Instance Properties ---
   imageX: number;
